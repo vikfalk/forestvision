a79c26bd014757bb2c92f557a9334a8e19bf2952
David Ammann <david.cosmo.ammann@gmail.com>
2024-05-28 17:26:12 +0200

    established a basic front end

--- Deforestation_Calculator.py
+++ Deforestation_Calculator.py
@@ -2,12 +2,11 @@ import datetime as dt
 import requests
 import pydeck as pdk
 import streamlit as st
-from shapely.geometry import Polygon
 
-PREDICTION_URL = 'http://localhost:8000/calculate_change'
+API_URL = 'http://localhost:8000/calculate_change'
 
-# TODO: Put the pointer yourself
-# TODO: Make it a dropdown with multiples of 512 * 10m
+# Enter "streamlit run Deforestation_Calculator.py" in the terminal to run it locally.
+# TODO: Functionality to allow the user to select the area on the map.
 
 st.set_page_config(
     page_title="Deforestation Tracker",
@@ -15,13 +14,13 @@ st.set_page_config(
 )
 
 st.sidebar.markdown("""
-    # Sidebar
+    # Sidebar Placeholder
     """)
 
 
 st.markdown('''
 # Deforestation Tracker
-Enter the the coordinates below and see how
+Enter the the coordinates below and see how the forest coverage has changed in the area of interest.
 ''')
 
 col1, col2, col3 = st.columns(3)
@@ -31,18 +30,11 @@ factors = [100, 50, 25, 10, 5, 4, 3, 2, 1]
 square_size_options = [5.12 * factor for factor in factors]
 square_size = col3.selectbox('Side Length of the Square in km', square_size_options)
 
-
-# location_input = st.text_input('Location', 'Optionally search by Location Name').lower()
-# if location_input:
-#     location_coordinates = get_coordinates(location_input)
-#     print("hello")
-
 col3, col4, col5 = st.columns(3)
 start_timeframe = col3.date_input('Start of Timeframe', dt.datetime(2021, 1, 1))
 end_timeframe = col4.date_input('End of Timeframe')
 sample_number = col5.number_input('Number of Samples', 1)
 
-# Map Functionality
 view_state = pdk.ViewState(
     longitude=longitude_input,
     latitude=latitude_input,
@@ -53,7 +45,7 @@ data = [
     {"position": [longitude_input, latitude_input], "name": "Location of Interest"},
 ]
 
-half_side_length = float(square_size) / 2 / 110.574  # Convert km to degrees
+half_side_length = float(square_size) / 2 / 110.574
 
 square_coords = [
     [longitude_input - half_side_length, latitude_input - half_side_length],
@@ -78,16 +70,6 @@ polygon_layer = pdk.Layer(
     extruded=False
 )
 
-# point_layer = pdk.Layer(
-#     'ScatterplotLayer',
-#     data=data,
-#     get_position='position',
-#     # radius_scale=10000,
-#     radius_min_pixels=10,
-#     get_fill_color=[255, 219, 88],
-#     pickable=True
-# )
-
 st.pydeck_chart(pdk.Deck(
         map_style='mapbox://styles/mapbox/satellite-v9',
         initial_view_state=view_state,
@@ -100,7 +82,8 @@ params = {
         'end_timeframe': end_timeframe,
         'longitude': longitude_input,
         'latitude': latitude_input,
-        'sample_number': sample_number
+        'sample_number': sample_number,
+        'square_size' : square_size
     }
 
 st.markdown("""
@@ -108,9 +91,8 @@ st.markdown("""
 """)
 st.json(params)
 
-# Prediction Functionality
 if st.button('Calculate Change') and all(params.values()):
-    response = requests.get(url=PREDICTION_URL, params=params, timeout=5)
+    response = requests.get(url=API_URL, params=params, timeout=5)
     prediction_json = response.json()
     prediction = round(prediction_json["change"], 2)
     prediction_string = format(prediction, '.2f')
@@ -124,4 +106,8 @@ else:
                 Please press the button.
                 """)
 
-# enter "streamlit run Deforestation_Calculator.py" in the terminal to run it locally.
+col1, col2 = st.columns(2)
+with col1:
+    st.image("./images/before_resized.png", caption="Before")
+with col2:
+    st.image("./images/after_resized.png", caption="After")
