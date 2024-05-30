3f781ca4cb1bc83f4b5396113e11e2b83a853470
David Ammann <david.cosmo.ammann@gmail.com>
2024-05-30 16:37:20 +0200

    new directory structure, file names and function names; more modular code; API functionalities based on user inputs;

---
+++ api/output_api.py
@@ -0,0 +1,91 @@
+from tensorflow.keras.models import load_model
+import numpy as np
+from PIL import Image
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from model.segmenter import segment
+from model.image_array_loaders import load_img_array_from_satellite, load_img_array_locally
+
+# INSTRUCTION:
+# Enter "uvicorn output_api:api_app --reload" in the command line.
+# Go to "http://localhost:8000/docs" to test it out.
+# Alternatively, run this file, triggering the __main__ function.
+
+api_app = FastAPI()
+
+@api_app.get('/')
+def index():
+    return {'api status': "running"}
+
+@api_app.get("/get_image")
+def get_image():
+    image_array = np.random.randint(0, 256, (3, 3, 3), dtype=np.uint8)
+    image_list = image_array.tolist()
+    return JSONResponse(content={"image_list": image_list})
+
+@api_app.get("/get_complex_image")  # bad name, but it will disappear soon.
+def get_complex_image():
+    image = Image.open('./segmented_output_files/after_resized.png')
+    image_array = np.array(image)
+    image_list = image_array.tolist()
+    return JSONResponse(content={"image_list": image_list})
+
+@api_app.get("/get_image_from_model")
+def get_image_from_model():
+    model = load_model('./model/model_ressources/unet-attention-3d.hdf5')
+    image_array = load_img_array_locally()
+    image_array = segment(image_array, model)
+    image_list = image_array.tolist()
+    return JSONResponse(content={"image_list": image_list})
+
+@api_app.get("/get_image_from_satellite")
+def get_image_from_satellite():
+    model = load_model('./model/model_ressources/unet-attention-3d.hdf5')
+    image_array = load_img_array_from_satellite()
+    image_array = segment(image_array, model)
+    image_list = image_array.tolist()
+    return JSONResponse(content={"image_list": image_list})
+
+@api_app.get("/get_image_from_satellite_with_params")
+def get_image_from_satellite_with_params(
+    start_timeframe: str,  # TODO: Pay attention to unused inputs.
+    end_timeframe: str,
+    longitude: str,
+    latitude: str,
+    sample_number: str,  # TODO: Pay attention to unused inputs.
+    square_size: str):  # TODO: Pay attention to unused inputs.
+    image_list = float(latitude), float(longitude), end_timeframe
+    model = load_model('./model/model_ressources/unet-attention-3d.hdf5')
+    image_array = load_img_array_from_satellite(
+        lat_deg=float(latitude),
+        lon_deg=float(longitude),
+        end_timeframe=str(end_timeframe)  # assuming format "2023-02-03"
+    )
+    image_array = segment(image_array, model)
+    image_list = image_array.tolist()
+    return JSONResponse(content={"image_list": image_list})
+
+@api_app.get("/generic_param_api")
+def generic_param_api(
+    start_timeframe,
+    end_timeframe,
+    longitude,
+    latitude,
+    sample_number,
+    square_size):
+    """To test the transmissions of UI parameters to the backend."""
+
+    param_list = {
+        "start_timeframe": start_timeframe,
+        "end_timeframe": end_timeframe,
+        "longitude": longitude,
+        "latitude": latitude,
+        "sample_number": sample_number,
+        "square_siz": square_size
+    }
+
+    return JSONResponse(content={"param_list": param_list})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(api_app, host="0.0.0.0", port=8000)
--- model/segmenter.py
+++ model/segmenter.py
@@ -1,29 +1,11 @@
-from tensorflow.keras.models import load_model
 from PIL import Image
 import numpy as np
 
-def path_constructor(image_name_without_ending="after_resized"):
-    """Hard coded function that returns paths to pulls in local .tiff file and output a .png file."""
-    input_file_ending = ".tiff"
-    output_file_ending = ".png"
-    image_name_ = image_name_without_ending
-    input_path_ = "./raw_data/our_images/" + image_name_ + input_file_ending
-    output_path_ = "./segmented_output_files/" + image_name_ + output_file_ending
-    return input_path_, output_path_
-
-def image_segmenter(source="local", clip=False): # destination="local"
-    model = load_model('./model/model_ressources/unet-attention-3d.hdf5')
-    if source=="local":
-        input_path, output_path = path_constructor("after_resized")
-        img = Image.open(input_path)
-    # else: # plug in satellite input_api:
-
-    if clip:
-        img_array = np.array(img) / 255.0 * 3.5
-        img_array = np.clip(a=img_array, a_min=0, a_max=1)
-    else:
-        img_array = np.array(img) / 255.0
-
+def segment(img_array: np.array, model) -> np.array:
+    """Takes a scaled image array, predicts its rainforest segmentation, converts it
+    to black and white and crops the images white padding. Returns a numpy
+    array representing the image.
+    """
     # adding batch size dimension
     prediction = model.predict(np.expand_dims(img_array, axis=0))
     predicted_image_array = prediction[0]
@@ -41,15 +23,6 @@ def image_segmenter(source="local", clip=False): # destination="local"
     bbox = binary_image_pil.getbbox()
     cropped_img = binary_image_pil.crop(bbox) if bbox else binary_image_pil
 
-    cropped_img.save(output_path)
+    # cropped_img.save(output_path)
     cropped_img_array = np.array(cropped_img, dtype=np.uint8)
     return cropped_img_array
-
-# to be specified whereever this function will be used:
-# model_ = load_model('./model/unet-attention-3d.hdf5')
-# input_file_ending = ".tiff"
-# output_file_ending = ".png"
-# image_name_ = input("What is the name of the image file in the folder 'our_images' (without ending)? ")
-# input_path_ = "./raw_data/our_images/" + image_name_ + input_file_ending
-# output_path_ = "./segmented_output_files/" + image_name_ + output_file_ending
-# image_segmenter(input_path_, output_path_, model_)
