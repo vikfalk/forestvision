61c5603603cd0685003b0f6d97b88578dc0def4b
vikfalk <viktorfalkengren@gmail.com>
2024-05-31 10:55:08 +0200

    Small update to make streamlit cloud work.

--- pages/Viktor_Test_Page.py
+++ pages/Viktor_Test_Page.py
@@ -3,7 +3,7 @@ import datetime as dt
 import pydeck as pdk
 from PIL import Image
 import requests
-import leafmap
+# import leafmap
 import random
 from streamlit_extras.grid import grid
 import streamlit_nested_layout
