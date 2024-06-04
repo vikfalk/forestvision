437914e766565559fe68d1875a0a0ebdb029bdad
vikfalk <viktorfalkengren@gmail.com>
2024-06-04 16:19:05 +0200

    ui changes

--- pages/Viktor_Test_Page.py
+++ pages/Viktor_Test_Page.py
@@ -165,27 +165,27 @@ with input_col1:
             
             #st.balloons()
 
-# with example_col:
-#     with st.container():
-#         img = image_select(
-#             "Choose an example",
-#             images=[
-#                 "https://vikfalk.github.io/deforestation_frontend/example_images/brazil.png",
-#                 "https://vikfalk.github.io/deforestation_frontend/example_images/brazil.png"
-#             ],
-#             captions=["Brazilian Soy Farm", "Brazilian Soy Farm"],
-#             return_value= 'index', 
-#             index = 0
-#         )
+with example_col:
+    with st.container():
+        img = image_select(
+            "Choose an example",
+            images=[
+                "https://vikfalk.github.io/deforestation_frontend/example_images/brazil.png",
+                "https://vikfalk.github.io/deforestation_frontend/example_images/brazil.png"
+            ],
+            captions=["Brazilian Soy Farm", "Brazilian Soy Farm"],
+            return_value= 'index', 
+            index = 0
+        )
     
-#         if img == 0:
-#             pass
+        if img == 0:
+            pass
             
-#         if img == 1:
-#             pass
-#             # st.session_state.longitude_input = -55.26000
-#             # st.session_state.latitude_input = -8.49000
-#             # st.session_state.zoom = 12.5
+        if img == 1:
+            pass
+            # st.session_state.longitude_input = -55.26000
+            # st.session_state.latitude_input = -8.49000
+            # st.session_state.zoom = 12.5
 
 
 # Calculate the map view state and polygon coordinates
