0943c5e7754a137dfc8d4e36350b017ccacf2b8c
David Ammann <david.cosmo.ammann@gmail.com>
2024-06-05 15:46:21 +0200

    multiple image requests

--- deforestation_tracker/main_copy.py
+++ deforestation_tracker/main_copy.py
@@ -6,8 +6,9 @@ from PIL import Image
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from deforestation_tracker.segmenter import segment, segment_self
-from deforestation_tracker.image_array_loaders import load_img_array_from_satellite, load_img_array_locally
+from deforestation_tracker.image_array_loaders import load_img_array_from_satellite, load_multiple_imgs_from_sat
 from deforestation_tracker.custom_layer import RepeatElements
+import datetime as dt
 
 # INSTRUCTION:
 # Run this file, triggering the __main__ function.
@@ -238,11 +239,56 @@ def do_everything_self(
                                  'end_date_info': end_date_info,
                                  })
 
+@app.get("/get_multiple_images_from_satellite")
+def get_multiple_images_from_satellite(
+    start_timeframe: str = "2020-05-13",
+    end_timeframe: str = "2024-05-30",
+    longitude: str = "-55.26209",
+    latitude: str = "-8.48638",
+    sample_number: str = "2"):
+
+    model = load_model('./deforestation_tracker/model_ressources/att_unet_4b.hdf5', custom_objects={'RepeatElements': RepeatElements})
+
+    # construct list with request dates
+    start_dt, end_dt = dt.datetime.strptime(start_timeframe, "%Y-%m-%d"), dt.datetime.strptime(end_timeframe, "%Y-%m-%d")
+    date_step_size = (end_dt - start_dt)/(int(sample_number) - 1)
+    date_list = [dt.date.strftime(start_dt + i * date_step_size, "%Y-%m-%d") for i in range(int(sample_number))]
+
+    # load both 3 band and 4 band images
+    date_list_loaded, img_list = load_multiple_imgs_from_sat(lat_deg=float(latitude), lon_deg=float(longitude), date_list=date_list, request_type='TrueColor')
+
+    # Join 3-band with 4-band
+    number_of_dates = len(date_list_loaded)
+    combined_img_arrays = []
+    for date in range(number_of_dates):
+        band_4_at_date = img_list[(date + number_of_dates)][:,:,3]
+        vis_at_date = img_list[date]
+
+        # Normalize 4-band channel to values between 0 and 1
+        max_value = np.max(band_4_at_date)
+        band_4_at_date = band_4_at_date / max_value
+        combined_img_arrays.append(np.dstack((vis_at_date, band_4_at_date)))
+
+    # Segment
+    segmented_arrays = [segment(img_at_date, model) for img_at_date in combined_img_arrays]
+    print(f'Shape of segmented arrays: {segmented_arrays[0].shape}')
+
+    # flatten image arrays
+    original_img_list = [img.flatten().tolist() for img in combined_img_arrays]
+    segmented_img_list = [mask.flatten().tolist() for mask in segmented_arrays]
+
+    # delete instances where arrays are not correct shape (512x512)
+    for i, list in enumerate(segmented_img_list):
+        if len(list) != 262144:
+            date_list_loaded.pop(i)
+            original_img_list.pop(i)
+            segmented_img_list.pop(i)
+
+    return JSONResponse(content = {
+        "date_list_loaded": date_list_loaded,
+        "original_img_list": original_img_list,
+        "segmented_img_list": segmented_img_list})
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ["PORT"]))
-    #latitude: float = -8.48638
-    #longitude: float = -55.26209
-    #end_timeframe: str = "2024-05-30"
-    #start_timeframe: str = "2020-05-30"
