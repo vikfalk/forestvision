5556b92fafd36f44a06d3748074e747d3c3ef9d6
David Ammann <david.cosmo.ammann@gmail.com>
2024-06-15 23:06:22 +0200

    simplified segment function as it does not need cropping with new model

--- deforestation_tracker/segmenter.py
+++ deforestation_tracker/segmenter.py
@@ -1,45 +1,9 @@
 import numpy as np
-from PIL import Image
 
-def segment(img_array: np.array, model) -> np.array:
+def segment(img_array: np.array, model, threshold = 0.5) -> np.array:
     """Takes a scaled image array, predicts its rainforest segmentation, converts it
-    to black and white and crops the images white padding. Returns a numpy
-    array representing the image.
+    to black and white according to a threshold. Returns a (512, 512) img_array.
     """
-    # adding batch size dimension
-    prediction = model.predict(np.expand_dims(img_array, axis=0))
-    predicted_image_array = prediction[0]
-
-    # converting to black and white
-    threshold = 0.5
-    binary_image_array = (predicted_image_array > threshold).astype(np.uint8) * 255
-
-    # reducing dimensions
-    if binary_image_array.ndim == 3 and binary_image_array.shape[-1] == 1:
-        binary_image_array = binary_image_array.squeeze(-1)
-    binary_image_pil = Image.fromarray(binary_image_array, mode='L') # 'L' mode is for (8-bit pixels, black and white)
-
-    # detect white padding/box and cropping
-    bbox = binary_image_pil.getbbox()
-    cropped_img = binary_image_pil.crop(bbox) if bbox else binary_image_pil
-
-    # cropped_img.save(output_path)
-    cropped_img_array = np.array(cropped_img, dtype=np.uint8)
-    return cropped_img_array
-
-
-def segment_self(img_array: np.array, model) -> np.array:
-    """Takes a scaled image array, predicts its rainforest segmentation, converts it
-    to black and white and crops the images white padding. Returns a numpy
-    array representing the image.
-    """
-    # adding batch size dimension
-    prediction = model.predict(np.expand_dims(img_array, axis=0))
-    prediction = prediction.reshape((512,512,1))
-
-    # converting predicted image to black and white
-    threshold = 0.7
-    prediction_array = (prediction > threshold).astype(np.uint8) * 255
-    prediction_array = prediction_array.astype(np.uint8)
-
-    return prediction_array
+    predicted_img_array = model.predict(np.expand_dims(img_array, axis=0)).reshape(512, 512)
+    black_white_img_array = (predicted_img_array > threshold).astype(np.uint8) * 255
+    return black_white_img_array
