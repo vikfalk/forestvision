b6a0f3ff1bf51cd8146b7f522ffe2b66c0c64abb
David Ammann <david.cosmo.ammann@gmail.com>
2024-06-18 00:21:26 +0200

    switched bw threshold to 0.7 again

--- deforestation_tracker/segmenter.py
+++ deforestation_tracker/segmenter.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def segment(img_array: np.array, model, threshold = 0.5) -> np.array:
+def segment(img_array: np.array, model, threshold = 0.7) -> np.array:
     """Takes a scaled image array, predicts its rainforest segmentation, converts it
     to black and white according to a threshold. Returns a (512, 512) img_array.
     """
